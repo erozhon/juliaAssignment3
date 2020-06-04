534aa3b5c7d2c89b5abbfcef84954892f4b541a1
Evan Rozhon <erozhon@users.noreply.github.com>
2020-06-03 17:19:57 -0700

    master plan

--- helloWorld.jl
+++ helloWorld.jl
@@ -1,2 +1,9 @@
 #this is a comment
 println("hello, world");
+
+# master plan
+# 1: take in a .aqse3 file
+# 2: parse (for lack of a better word) the file into something we can interp?
+# 3: interp the file
+# 4: ???
+# 5 : profit
