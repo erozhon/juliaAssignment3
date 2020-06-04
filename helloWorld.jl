749f8dcfcaf3cb7acc6c728ad5f5513fbf3a1a41
Evan Rozhon <erozhon@users.noreply.github.com>
2020-06-03 17:57:52 -0700

    ExprC stuff

--- helloWorld.jl
+++ helloWorld.jl
@@ -2,8 +2,33 @@
 println("hello, world");
 
 # master plan
-# 1: take in a .aqse3 file
-# 2: parse (for lack of a better word) the file into something we can interp?
-# 3: interp the file
+# 1: make the data structures
+# 2: pass them into stuff
+# 3: interp the things
 # 4: ???
 # 5 : profit
+
+#EXPRC TYPES, no idea how to make them reference each other explicity, maybe it doesn't matter!
+struct numC
+        number
+end
+struct strC
+        string
+end
+struct AppC
+        fExprC
+        argExprCList
+end
+struct ifC
+        ifExprC
+        thenExprC
+        elseExprC
+end
+struct lamC
+        argExprC
+        bodyExprc
+end
+
+
+@test interp("5") == 5
+@test interp("hello") == "hello"
